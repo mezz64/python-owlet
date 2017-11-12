318707a8d3a3f30013bfa9a72d46178104ecc943
Tristan Escalada <tristan@escalada.us>
2017-11-12 14:08:10 -0500

    converting to a class
    not changing any functionality yet,
    just converting to a class

--- owlet_stats.py
+++ owlet_stats.py
@@ -6,87 +6,98 @@ import requests
 
 __author__ = 'fgorodishter'
 
-headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-}
-auth_header = {
-    'Authorization': 'auth_token ***'
-}
-properties_url = 'https://ads-field.aylanetworks.com/apiv1/dsns/AC********/properties'
 
-properties = [
-    'OXYGEN_LEVEL',
-    'HEART_RATE',
-]
+class Owlet(object):
 
-auth_token = ''
-expire_time = 0
-last_time = ''
-
-
-def login():
-    global auth_token, expire_time
-    login_url = 'https://user.aylanetworks.com:443/users/sign_in.json'
-    login_payload = {
-      "user": {
-        "email": "EMAIL",
-        "password": "PASS",
-        "application": {
-          "app_id": "OWL-id",
-          "app_secret": "OWL-4163742"
+    def __init__(self):
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         }
-      }
-    }
+        self.auth_header = {
+            'Authorization': 'auth_token ***'
+        }
+        self.properties_url = 'https://ads-field.aylanetworks.com/apiv1/dsns/AC********/properties'
 
-    # print (auth_token, expire_time, time.time())
-    if (auth_token == '') or (expire_time <= time.time()):
-        print ('Generating token')
-        data = requests.post(login_url, json=login_payload, headers=headers)
-        auth_token = data.json()['access_token']
-        expire_time = time.time() + 3600
-        print (auth_token)
+        self.properties = [
+            'OXYGEN_LEVEL',
+            'HEART_RATE',
+        ]
 
+        self.auth_token = ''
+        self.expire_time = 0
+        self.last_time = ''
 
-while True:
-    login()
-    time.sleep(1)
+    def login(self):
+        login_url = 'https://user.aylanetworks.com:443/users/sign_in.json'
+        login_payload = {
+          "user": {
+            "email": "EMAIL",
+            "password": "PASS",
+            "application": {
+              "app_id": "OWL-id",
+              "app_secret": "OWL-4163742"
+            }
+          }
+        }
 
-    auth_header = {
-        'Authorization': 'auth_token ' + auth_token
-    }
-    auth_header.update(headers)
-
-    output = {}
-    for measure in properties:
-        url = properties_url + '/' + measure
-        data = requests.get(url, headers=auth_header)
-        # woo = dump.dump_all(data)
-        # print(woo.decode('utf-8'))
-        # print(data, data.text, data.headers)
-        data_json = data.json()
-        if data_json['property']['data_updated_at'] == last_time:
-            print('.')
-        else:
-            print (
-                measure,
-                data_json['property']['value'],
-                data_json['property']['data_updated_at']
+        # print (auth_token, expire_time, time.time())
+        if (self.auth_token == '') or (self.expire_time <= time.time()):
+            print ('Generating token')
+            data = requests.post(
+                login_url,
+                json=login_payload,
+                headers=self.headers
             )
-            output.update(
-                {
-                    'measure': data_json['property']['value'],
-                    'data_updated_at': data_json['property']['data_updated_at']
-                }
+            self.auth_token = data.json()['access_token']
+            self.expire_time = time.time() + 3600
+            print (self.auth_token)
+
+    def get_data(self):
+        while True:
+            self.login()
+            time.sleep(1)
+
+            self.auth_header = {
+                'Authorization': 'auth_token ' + self.auth_token
+            }
+            self.auth_header.update(self.headers)
+
+            output = {}
+            for measure in self.properties:
+                url = self.properties_url + '/' + measure
+                data = requests.get(url, headers=self.auth_header)
+                # woo = dump.dump_all(data)
+                # print(woo.decode('utf-8'))
+                # print(data, data.text, data.headers)
+                data_json = data.json()
+                if data_json['property']['data_updated_at'] == self.last_time:
+                    print('.')
+                else:
+                    print (
+                        measure,
+                        data_json['property']['value'],
+                        data_json['property']['data_updated_at']
+                    )
+                    output.update(
+                        {
+                            'measure': data_json['property']['value'],
+                            'data_updated_at': data_json['property']['data_updated_at']
+                        }
+                    )
+
+            # log to es
+            requests.post(
+                'http://ES_ADDRESS:9200/owlet/measure/' + output['data_updated_at'],
+                verify=False,
+                json=output,
+                headers=self.headers
             )
 
-    # log to es
-    requests.post(
-        'http://ES_ADDRESS:9200/owlet/measure/' + output['data_updated_at'],
-        verify=False,
-        json=output,
-        headers=headers
-    )
+            # if we want to remove duplicate posts, uncomment and fix this
+            # last_time = data.json()['property']['data_updated_at']
 
-    # if we want to remove duplicate posts, uncomment and fix this
-    # last_time = data.json()['property']['data_updated_at']
+
+if __name__ == "__main__":
+    o = Owlet()
+    o.get_data()
